948ac54bb876d67be68f3421e86eca6aad590d52
Manish Kumar <manishjanky@gmail.com>
2026-02-10 16:24:07 +0530

    PWA Support added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import million from "million/compiler";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [million.vite({auto: true}),tailwindcss(), react()],
-})
+  plugins: [
+    million.vite({ auto: true }),
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        description: 'This application provides your with some financial tools that help you with your day to financial needs in a secure manner. ',
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
