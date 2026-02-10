6b6c9bc88df31363d8155143dc9c43c07f7a9a13
Manish Kumar <manishjanky@gmail.com>
2026-02-10 18:10:58 +0530

    PWA setup completed

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,22 @@ export default defineConfig({
       registerType: "autoUpdate",
       devOptions: { enabled: true },
       manifest: {
-        description: 'This application provides your with some financial tools that help you with your day to financial needs in a secure manner. ',
+        description:
+          "This application provides your with some financial tools that help you with your day to financial needs in a secure manner. ",
+        theme_color: "#6366F1",
+        background_color: "#ffffff",
+        display: "standalone",
+        name: "fin2ools",
+        short_name: "fin2ools",
+        start_url: "/",
+        screenshots: [
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            form_factor: 'wide'
+          },
+        ],
         icons: [
           {
             src: "pwa-64x64.png",
