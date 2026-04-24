ab7676d0a349a3955c2273b6284b282ebfed5820
Manish Kumar <manishjanky@gmail.com>
2026-04-24 09:51:53 +0530

    NAV history not available

--- src/modules/mutual-funds/components/SchemeDetails.tsx
+++ src/modules/mutual-funds/components/SchemeDetails.tsx
@@ -62,7 +62,7 @@ export default function SchemeDetails() {
         };
 
         loadData();
-    }, [schemeCode, getOrFetchSchemeDetails, getOrFetchSchemeHistory]);
+    }, [schemeCode]);
 
     if (loading) {
         return (
